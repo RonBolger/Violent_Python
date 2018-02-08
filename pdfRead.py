c3fd8a8b0e876d6faf82046c3cdaf91216b64f31
rbolger <ronbolger@gmail.com>
2018-02-08 10:33:18 -0600

    Updated with comments

---
+++ pdfRead.py
@@ -0,0 +1,26 @@
+#This script will read metadata from a PDF file and print out the results.
+import PyPdf
+import optparse
+from PyPdf import PdfFileReader
+
+def printMeta(fileName):
+    pdfFile = PdfFileReader(file(fileName, 'rb'))
+    docInfo = pdfFile.getDocumentInfo()
+    print '[+] PDF MetaData For: ' + str(fileName)
+    for metaItem in docInfo:
+        print '[+] ' + metaItem + ':' + docInfo[metaItem]
+
+def main():
+    parser = optparse.OptionParser('usage %prog "+\"-F <PDF file name>')
+    parser.add_option('-F', dest='fileName', type='string',\
+                      help='specify PDF file name')
+    (options, args) = parser.parse_args()
+    fileName = options.fileName
+    if fileName == None:
+        print parser.usage
+        exit(0)
+    else:
+        printMeta(fileName)
+
+if __name__ == '__main__':
+    main()
